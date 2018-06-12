c03f3870952188b33c80e2872930e0dcfe13465f
MartaEN <marta.the.developer@gmail.com>
2018-06-12 13:41:21 +0300

    Just basic test client to show how table view works. Copied the main client files and deleted all the network stuff.

---
+++ client/src/client/TestClientNoConnection/Main.java
@@ -0,0 +1,24 @@
+package TestClientNoConnection;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    private Stage stage;
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("test.fxml")); //вот тут пришлось добавить getClassLoader(), иначе не получилось грузить fxml
+        primaryStage.setTitle("GeekCloud Client");
+        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.show();
+        stage = primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+}
